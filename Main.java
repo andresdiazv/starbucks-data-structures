ff4a044e41e2ed86c77bb5661094d11d120e466e
Andres Diazvictores <adiaz616@fiu.edu>
2023-04-17 15:03:35 -0400

    fresh start

--- Main.java
+++ Main.java
@@ -1,23 +1,4 @@
 public class Main {
     public static void main(String[] args) {
-        // Create inventory
-        Inventory inventory = new Inventory();
-
-        // Add products to inventory
-        inventory.addProduct("Coffee", 10);
-        inventory.addProduct("Tea", 5);
-
-        // Create a new menu
-        Menu menu = new Menu(inventory);
-
-        // Create a new order
-        Order order = new Order(menu);
-
-        // Add items to order
-        order.addItem("Coffee", 2);
-        order.addItem("Tea", 1);
-
-        // Print order summary
-        System.out.println(order.getSummary());
     }
 }
--- Order.java
+++ Order.java
@@ -2,58 +2,5 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class Order {
-    private Menu menu; // Menu object for checking prices and product availability
-    private Queue<Item> items; // Queue for managing incoming orders
-
-    public Order(Menu menu) {
-        this.menu = menu;
-        items = new LinkedList<>();
-    }
-
-    public void addItem(String name, int quantity) {
-        if (menu.checkAvailability(name, quantity)) {
-            items.offer(new Item(name, quantity));
-            menu.updateInventory(name, quantity);
-        } else {
-            System.out.println("Sorry, we are out of stock for " + name);
-        }
-    }
-
-    public double getTotal() {
-        double total = 0;
-        for (Item item : items) {
-            total += menu.getPrice(item.getName()) * item.getQuantity();
-        }
-        return total;
-    }
-
-    public String getSummary() {
-        StringBuilder summary = new StringBuilder();
-        summary.append("Order Summary:\n");
-        for (Item item : items) {
-            summary.append(item.getQuantity()).append(" x ").append(item.getName())
-                    .append(" @ $").append(menu.getPrice(item.getName())).append(" = $")
-                    .append(menu.getPrice(item.getName()) * item.getQuantity()).append("\n");
-        }
-        summary.append("Total: $").append(getTotal());
-        return summary.toString();
-    }
-
-    private static class Item {
-        private String name;
-        private int quantity;
-
-        public Item(String name, int quantity) {
-            this.name = name;
-            this.quantity = quantity;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public int getQuantity() {
-            return quantity;
-        }
-    }
+    
 }
